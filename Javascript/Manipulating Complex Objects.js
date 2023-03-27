5132fa5ab2e540233da2e132ec32621cd9ea0afc
Abraham Mejía Serrano <98870996+AbrahamMejiaSerrano@users.noreply.github.com>
2023-03-27 11:53:31 -0500

    work

---
+++ Javascript/Manipulating Complex Objects.js
@@ -0,0 +1,16 @@
+var myMusic = [
+    {
+      artist: "Billy Joel",
+      title: "Piano Man",
+      release_year: 1973,
+      formats: ["CD", "8T", "LP"],
+      gold: true
+    },
+    // Add record here
+    {
+      artist: "",
+      title: "",
+      release_year "",
+      formats: ["CD", "8T", "LP"]
+    }
+  ];
